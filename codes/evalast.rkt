3cff0b329372d5f86a6b5f15b6e181e834256c8e
Groverkss <groverkss@gmail.com>
2023-09-17 18:30:19 +0530

    Revert "Lecture 7 (#9)"
    This reverts commit 5ae01d2c6edfbb10a998c86f690ebc117c4a0f8d.

--- codes/evalast.rkt
+++
@@ -1,8 +0,0 @@
-(define (eval-ast e)
-  (match e
-	[(number?) a]
-	[(list + e1 e2)
-	 	(let ([a1 (eval-ast e1)]
-		      [a2 (eval-ast e2)])
-		  (+ a1 a2))]))
-;; similarly there would be cases for -, *
